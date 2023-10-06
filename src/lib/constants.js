8ed1ccb59367b8587abd4b48951cd5f800643bde
Abdulsemiu Atanda <semiu@live.com>
2023-10-06 18:16:12 -0400

    add constants file

---
+++ src/lib/constants.js
@@ -0,0 +1,41 @@
+export const LINKS = [
+  {
+    id: '1',
+    path: '/',
+    label: 'Home'
+  },
+  {
+    id: '2',
+    path: 'events',
+    label: 'Events'
+  },
+  {
+    id: '3',
+    path: 'services',
+    label: 'Services'
+  },
+  {
+    id: '4',
+    path: 'healing-corner',
+    label: 'Healing Corner'
+  },
+  {
+    id: '5',
+    path: 'press',
+    label: 'Funders/Press'
+  },
+  {
+    id: '6',
+    path: 'work-with-us',
+    label: 'Work With Us'
+  }
+]
+
+export const QUICK_LINKS = [
+  ...LINKS,
+  {
+    id: '7',
+    path: 'contact',
+    label: 'Contact Us'
+  }
+]
