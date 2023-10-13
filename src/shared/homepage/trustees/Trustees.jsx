c965a79f32539e4a011e6a04285973cd6478ece1
Abdulsemiu Atanda <semiu@live.com>
2023-10-12 22:42:35 -0400

    add trustees to home page

--- src/shared/homepage/trustees/Trustees.jsx
+++ src/shared/homepage/trustees/Trustees.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import {useStaticQuery, graphql} from 'gatsby'
 
-// import {TRUSTEES} from '../../../graphql/queries'
+import {GatsbyImage} from 'gatsby-plugin-image'
+
+import '../../../styles/trustees.scss'
 
 const Trustees = () => {
   const data = useStaticQuery(graphql`
@@ -13,7 +15,7 @@ const Trustees = () => {
             asset {
               id
               title
-              url
+              gatsbyImageData
             }
           }
           name
@@ -24,7 +26,26 @@ const Trustees = () => {
   `)
   console.log(data)
 
-  return null
+  return (
+    <section className='trustees'>
+      <p className='text'>Get Inspired</p>
+      <h1 className='heading'>Team + Trustees</h1>
+      <div className='image-container'>
+        {
+          data.allSanityTrustee.nodes.map(
+            trustee => (
+              <GatsbyImage
+                key={trustee.id}
+                className='trustee-image'
+                alt={trustee.name}
+                image={trustee.image.asset.gatsbyImageData} 
+              />
+            )
+          )
+        }
+      </div>
+    </section>
+  )
 }
 
 export default Trustees
