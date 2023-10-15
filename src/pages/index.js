20caa2ea0a291dbbf2795d477c7cb3b1e409b25e
Abdulsemiu Atanda <semiu@live.com>
2023-10-15 12:41:22 -0400

    use variable name on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,13 +7,15 @@ import Mission from "../shared/Mission"
 import Showcases from "../shared/homepage/showcases/Showcases"
 import Trustees from "../shared/homepage/trustees/Trustees"
 
+import {ORGANIZATION_NAME} from '../lib/constants'
+
 import "../styles/homepage.scss"
 
 const IndexPage = () => {
   return (
     <main className="homepage">
       <Header />
-      <h1 className="base-heading">HomeGirlsUnite</h1>
+      <h1 className="base-heading">{ORGANIZATION_NAME.replace(/\s/g, '')}</h1>
       <h4 className="base-sub-heading">Creating Safe Spaces For Eldest Daughters</h4>
       <Showcases />
       <Trustees />
