0ac0617f98ee4be7b2d08bccf696220539860b73
Abdulsemiu Atanda <semiu@live.com>
2023-10-22 19:05:18 -0400

    update copyright

--- src/shared/footer/Copyright.jsx
+++ src/shared/footer/Copyright.jsx
@@ -1,9 +1,6 @@
 import * as React from 'react'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faInstagram} from '@fortawesome/free-brands-svg-icons'
 
-import TwitterIcon from '../../images/TwitterIcon'
-import TiktokIcon from '../../images/TiktokIcon'
+import Socials from '../Socials'
 
 import '../../styles/footer/copyright.scss'
 
@@ -15,17 +12,7 @@ const Copyright = () => {
       <span>
         © {date.getFullYear()} HGU All Rights Reserved.
       </span>
-      <div className='socials'>
-        <a href='https://www.instagram.com/homegirlsunite' target='_blank' rel='noreferrer'>
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
-        <a href='https://twitter.com/HomeGirlsUnite' target='_blank' rel='noreferrer'>
-          <TwitterIcon />
-        </a>
-        <a href='https://www.tiktok.com/@homegirlsunite' target='_blank' rel='noreferrer'>
-          <TiktokIcon />
-        </a>
-      </div>
+      <Socials />
     </div>
   )
 }
