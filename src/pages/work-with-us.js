b2e32be4717a9c2643e365afc05ed04aabb10b66
Abdulsemiu Atanda <semiu@live.com>
2023-10-21 23:55:18 -0400

    add work with us page

--- src/pages/work-with-us.js
+++ src/pages/work-with-us.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
-const WorkWithUsPage = () => (<div />)
+import PageSections from '../shared/pages/PageSections'
+
+const WorkWithUsPage = ({pages, uri}) => {
+  const data = pages.find(page => page.slug.current === uri.replace('/', '')) || {}
+
+  return (
+    <React.Fragment>
+      <h1 className='base-heading'>{data.name}</h1>
+      <PageSections sections={data.sections} className='work-with-us' />
+    </React.Fragment>
+  )
+}
 
 export default WorkWithUsPage
