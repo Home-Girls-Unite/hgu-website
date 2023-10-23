dff7aa53a874c4fe331ed232019a661dba1b3bc6
Abdulsemiu Atanda <semiu@live.com>
2023-10-22 21:41:58 -0400

    add sentence to uppercase helper

--- src/lib/textTools.js
+++ src/lib/textTools.js
@@ -1,5 +1,15 @@
 export const firstLetterUppercase = word => (word[0].toUpperCase() + word.substring(1))
 
+export const sentenceToUppercase = sentence => {
+  const words = sentence.split(' ')
+
+  return words.reduce((accumulator, word) => {
+    accumulator = [...accumulator, firstLetterUppercase(word)]
+
+    return accumulator
+  }, []).join(' ')
+}
+
 export const normalizeUrl = path => {
   if (path.startsWith('/'))
     return path
