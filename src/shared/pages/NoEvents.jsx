5dd500cefda530b4f99eb3acdd41a6d1386b8cf7
Abdulsemiu Atanda <semiu@live.com>
2023-10-28 19:52:57 -0400

    use background image in no events

--- src/shared/pages/NoEvents.jsx
+++ src/shared/pages/NoEvents.jsx
@@ -2,22 +2,19 @@ import React from 'react'
 
 import {PortableText} from '@portabletext/react'
 
+import BackgroundImage from './BackgroundImage'
 import Socials from '../Socials'
 
 import '../../styles/pages/noEvents.scss'
 
 const NoEvents = ({page}) => (
-  <section
-    className='no-events'
-    style={{
-      backgroundImage: `url(${page.cover.image.asset.url})`
-    }}>
-      <div className='description'>
-        <h1>{page.sections[0].title}</h1>
-        <PortableText value={page.sections[0]._rawDescription} />
-        <Socials />
-      </div>
-  </section>
+  <BackgroundImage className='no-events' imageUrl={page.cover.image.asset.url}>
+    <div className='description'>
+      <h1>{page.sections[0].title}</h1>
+      <PortableText value={page.sections[0]._rawDescription} />
+      <Socials />
+    </div>
+  </BackgroundImage>
 )
 
 export default NoEvents
