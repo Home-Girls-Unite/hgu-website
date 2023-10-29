45af67c7e637f04c7954170269d0cdd1c5fd50c2
Abdulsemiu Atanda <semiu@live.com>
2023-10-28 20:33:26 -0400

    use page unavailable in No Events

--- src/shared/pages/NoEvents.jsx
+++ src/shared/pages/NoEvents.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
 
-import {PortableText} from '@portabletext/react'
-
-import BackgroundImage from './BackgroundImage'
-import Socials from '../Socials'
-
-import '../../styles/pages/noEvents.scss'
+import PageUnavailable from './PageUnavailable'
 
 const NoEvents = ({page}) => (
-  <BackgroundImage className='no-events' imageUrl={page.cover.image.asset.url}>
-    <div className='description'>
-      <h1>{page.sections[0].title}</h1>
-      <PortableText value={page.sections[0]._rawDescription} />
-      <Socials />
-    </div>
-  </BackgroundImage>
+  <PageUnavailable
+    className='no-events'
+    imageUrl={page.cover.image.asset.url}
+    title={page.sections[0].title}
+    description={page.sections[0]._rawDescription}
+    type='dynamic'
+  />
 )
 
 export default NoEvents
