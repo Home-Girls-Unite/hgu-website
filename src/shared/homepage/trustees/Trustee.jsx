d8e882d91f115d480f909ae1ddd2addf7585e4d4
Abdulsemiu Atanda <semiu@live.com>
2023-11-12 01:41:37 -0400

    remove unnecessary elemnt control

--- src/shared/homepage/trustees/Trustee.jsx
+++ src/shared/homepage/trustees/Trustee.jsx
@@ -1,33 +1,18 @@
-import React, {useCallback, useState} from 'react'
-import classnames from 'classnames'
+import React from 'react'
 import {GatsbyImage} from 'gatsby-plugin-image'
 
-const Trustee = ({trustee}) => {
-  const [showNamePlate, setShowNamePlate] = useState(false)
-
-  const onMouseEnter = useCallback(event => {
-    event.preventDefault()
-    setShowNamePlate(true)
-  }, [])
-
-  const onMouseLeave = useCallback(event => {
-    event.preventDefault()
-    setShowNamePlate(false)
-  }, [])
-
-  return (
-    <div className='trustee' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
-      <div className={classnames('name-plate', {display: showNamePlate})}>
-        <h2>{trustee.name}</h2>
-        <p>{trustee.role}</p>
-      </div>
-      <GatsbyImage
-        className={classnames('trustee-image', {zoom: showNamePlate})}
-        alt={trustee.name}
-        image={trustee.image.asset.gatsbyImageData} 
-      />
+const Trustee = ({trustee}) => (
+  <div className='trustee'>
+    <div className='name-plate'>
+      <h2>{trustee.name}</h2>
+      <p>{trustee.role}</p>
     </div>
-  )
-}
+    <GatsbyImage
+      className='trustee-image'
+      alt={trustee.name}
+      image={trustee.image.asset.gatsbyImageData} 
+    />
+  </div>
+)
 
 export default Trustee
