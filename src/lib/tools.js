0f8e407c176471e903df9a6afdac0cbda43b2d43
Abdulsemiu Atanda <semiu@live.com>
2023-11-11 21:05:10 -0400

    compare year and month

--- src/lib/tools.js
+++ src/lib/tools.js
@@ -2,7 +2,7 @@ export const activeEvent = events => events.find(event => {
   const eventDate = new Date(event.eventDate)
   const now = new Date(Date.now())
 
-  return eventDate.getUTCMonth() === now.getUTCMonth()
+  return eventDate.getUTCFullYear() === now.getUTCFullYear() && eventDate.getUTCMonth() === now.getUTCMonth()
 })
 
 export const findBySlug = ({list, slug}) => (list.find(node => node.slug.current === slug) || {})
