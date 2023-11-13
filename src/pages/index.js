aec30a763dcc3a14d97c82cdbbbe6b6a2eed9185
Abdulsemiu Atanda <semiu@live.com>
2023-11-13 01:44:11 -0400

    display book session on scroll to bottom

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
-import * as React from "react"
+import React, {useState} from "react"
 
+import BookSession from "../shared/pages/BookSession"
 import Information from "../shared/homepage/Information"
 import Mission from "../shared/Mission"
 import Showcases from "../shared/homepage/showcases/Showcases"
@@ -8,16 +9,47 @@ import Trustees from "../shared/homepage/trustees/Trustees"
 
 import {ORGANIZATION_NAME} from '../lib/constants'
 
-const IndexPage = ({photos}) => (
-  <React.Fragment>
-    <h1 className="base-heading">{ORGANIZATION_NAME.replace(/\s/g, '')}</h1>
-    <h4 className="base-sub-heading">Creating Safe Spaces For Eldest Daughters</h4>
-    <Showcases />
-    <Information photos={photos} />
-    <Listen />
-    <Trustees />
-    <Mission />
-  </React.Fragment>
-)
+const IndexPage = ({photos}) => {
+  const [seenDialog, setSeenDialog] = useState(false)
+  const [showDialog, setShowDialog] = useState(false)
+
+  const handleScroll = () => {
+    const bottom = Math.ceil(window.innerHeight + window.scrollY) >= document.documentElement.scrollHeight
+
+    if (bottom && !seenDialog)
+      setShowDialog(true)
+  }
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  const dismissDialog = event => {
+    if (event)
+      event.preventDefault()
+
+    setSeenDialog(true)
+    setShowDialog(false)
+  }
+
+  return (
+    <React.Fragment>
+      <h1 className="base-heading">{ORGANIZATION_NAME.replace(/\s/g, '')}</h1>
+      <h4 className="base-sub-heading">Creating Safe Spaces For Eldest Daughters</h4>
+      <Showcases />
+      <Information photos={photos} />
+      <Listen />
+      <Trustees />
+      <Mission />
+      {
+        (!seenDialog && showDialog) && <BookSession dismissDialog={dismissDialog} />
+      }
+    </React.Fragment>
+  )
+}
 
 export default IndexPage
